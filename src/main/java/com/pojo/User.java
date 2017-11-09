48f4383b58037cdd2b23045b825664f5bdd9d4af
王超 <wangchao@utourworld.com>
2017-11-09 17:40:15 +0800

    新建項目

---
+++ src/main/java/com/pojo/User.java
@@ -0,0 +1,14 @@
+package com.pojo;
+
+/**
+ * Author:   wangchao
+ * Version：
+ * Date:     2017/11/9
+ * Description:
+ * Modification  History:
+ * Date         	Author        		Version        	Description
+ * --------------------------------------------------------------
+ * Why & What is modified:
+ */
+public class User {
+}
