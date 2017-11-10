952ad2eddb1eabd0c1e458fa55661f1afd815a9d
王超 <wangchao@utourworld.com>
2017-11-10 10:44:26 +0800

    新建項目

---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+/**
+ * Author:   wangchao
+ * Version：
+ * Date:     2017/11/10
+ * Description:
+ * Modification  History:
+ * Date         	Author        		Version        	Description
+ * --------------------------------------------------------------
+ * Why & What is modified:
+ */
+public class UserController {
+}
